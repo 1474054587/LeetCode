bd6b35a214971ec1e5d266ffcd2fc66dc133fccf
1474054587 <79830619+1474054587@users.noreply.github.com>
2022-04-02 11:07:42 +0800

    Update Week284.java

--- Week284.java
+++ Week284.java
@@ -1,10 +1,17 @@
-package leetcode;
+package LeetCode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Week284 {
-    //2200.(简单) 找出数组中的所有 K 近邻下标
+    /*
+    2200.(简单) 找出数组中的所有 K 近邻下标
+    思路
+    1.顺序找出每个 nums[ j ] == key3.
+    2.end 为 最小值（j + k ，nums最后一位），防止数组越界
+    3.start 为 最大值（j - k ，上一个end），避免重复
+    4.将 start 到 end 放入 ArrayList，返回ArrayList
+     */
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
         int start = 0;
         int end = 0;
@@ -21,7 +28,14 @@ public class Week284 {
         }
         return list;
     }
-    //2201. (中等)统计可以提取的工件
+    /*
+    2201. (中等)统计可以提取的工件
+    思路
+    1.新建一个二维数组[n][n]，代表网格
+    2.所有 dig 的位置，网格=1
+    3.如果一个 artifacts 的所有位置，网格都为1，则这个 artifacts 可以被挖出
+    4.判断所有 artifacts ，返回可挖出数量
+     */
     public int digArtifacts(int n, int[][] artifacts, int[][] dig) {
         int[][] map = new int[n][n];
         int num = 0;
